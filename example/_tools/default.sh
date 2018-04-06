7632584582d336e8e782488ae821bf46cb96b22f
Alexar <github@alexar.me>
2018-04-06 14:42:20 +1000

    Adds a working example to demo all the options.

---
+++ example/_tools/default.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "[Help]"
+echo "This script supports the following commands:"
+echo "- hello: Says hello."
+
---
+++ example/toolset.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+taskrunner_prefix="_tools/"
+taskrunner_suffix=".sh"
+
+. ../bin/taskrunner
